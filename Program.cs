98bbded5d0ded57d80d33c4d889eee83fc2dab5e
Viktorya <79201287827@yandex.ru>
2023-03-29 19:40:17 +0300

    Задача 19

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿Console.Clear( );
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-if (n < 100)
-    Console.WriteLine ("Третьей цифры нет");
-else
-{
-    while (n > 1000)
-    n = n / 10;
-    Console.WriteLine (n % 10);
-}
+Console.WriteLine("Введите число: ");
+string n = Console.ReadLine( );
+
+if (n[0] == n[4] && n[1] == n[3])
+    Console.WriteLine ( "Да" );
+else 
+    Console.WriteLine ( "Нет" );
