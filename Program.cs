e9213db0d91c4736f6675134e7e2ec1e1ee12d08
Viktorya <79201287827@yandex.ru>
2023-03-29 20:06:55 +0300

    Задача 21

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
-﻿Console.Clear( );
-Console.WriteLine("Введите число: ");
-string n = Console.ReadLine( );
+﻿Console.Clear();
+Console.Write ("x1= ");
+double x1 = Convert.ToDouble(Console.ReadLine ());
+Console.Write ("y1= ");
+double y1 = Convert.ToDouble(Console.ReadLine ());
+Console.Write ("z1= ");
+double z1 = Convert.ToDouble(Console.ReadLine ());
+Console.Write ("x2= ");
+double x2 = Convert.ToDouble(Console.ReadLine ());
+Console.Write ("y2= ");
+double y2 = Convert.ToDouble(Console.ReadLine ());
+Console.Write ("z2= ");
+double z2 = Convert.ToDouble(Console.ReadLine ());
 
-if (n[0] == n[4] && n[1] == n[3])
-    Console.WriteLine ( "Да" );
-else 
-    Console.WriteLine ( "Нет" );
+Console.WriteLine 
+(Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2)));
